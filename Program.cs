d1965d5c12272468a23dc3b3586a8294651fdd3a
Vahdet Savci <savcivahdet@gmail.com>
2023-10-20 16:23:42 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace if_else_if;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int hour = DateTime.Now.Hour;
+        
+        if(hour >= 6 && hour < 11)
+            Console.WriteLine("Günaydın!");
+        else if(hour <= 18)
+            Console.WriteLine("İyi günler!");
+        else
+            Console.WriteLine("İyi geceler!");
+
+        // string sonuc = hour <= 18 ? "İyi günler!" : "İyi geceler";
+        string sonuc = hour >= 6 && hour < 11 ? "Günaydın!" : hour <= 18 ? "İyi günler!" : "İyi geceler!";
+        Console.WriteLine(sonuc);
+    }
+}
